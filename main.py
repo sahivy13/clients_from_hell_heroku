05939d516bb0aa114f98bc6a6979fade96350edf
sahivy13 <sahivy1990@gmail.com>
2020-09-27 02:01:18 -0400

    adding creation of tables for db

--- main.py
+++ main.py
@@ -143,6 +143,97 @@ def save_all_or_one(process_dict):
         with open (f'saved_models/{model_selection}.pickle', 'wb') as f:
             pickle.dump(model_chosen,f)
 
+def create_db_tables(df_models):
+    # Hey future Sahivy, This is past Sahivy.
+    # This function requires to add a way to create 
+    # the params table based of params used...
+    # This is your job, have fun nerd! 
+    #      ."".   ."",
+    #      |  |  /  /
+    #      |  | /  /
+    #      |  |/  ;-._
+    #      }  ` _/  / ;
+    #      |  /` ) /  /
+    #      | /  /_/\_/\
+    #       (   \ '-  |
+    #       \    `.  /
+    #        |      |   "SUCKS TO SUCK!"
+
+
+    import psycopg2
+    conn = psycopg2.connect(
+        user = "onpsmhcjnzdsiz",
+        password = "54cad954a541572fa5b79d1cd9448b4c2971306246824c1f9468c853ef6471b0",
+        host = "ec2-3-216-92-193.compute-1.amazonaws.com",
+        port = "5432", #Postgres Port
+        database = "dc1fq03u49u20u"
+    )
+
+    cur = conn.cursor()
+
+    sql_params_table = """
+    CREATE TABLE IF NOT EXISTS params (
+        id SERIAL PRIMARY KEY,
+        c REAL,
+        n_neighbors SMALLINT,
+        alpha REAL,
+        ccp_alpha REAL,
+        max_features VARCHAR(100),
+        n_estimators REAL
+    );
+    """
+
+    sql_names_table = """
+    CREATE TABLE IF NOT EXISTS model_names (
+        id SERIAL PRIMARY KEY,
+        model_name VARCHAR(100) UNIQUE NOT NULL
+    );
+    """
+
+    sql_main_table = """
+    CREATE TABLE IF NOT EXISTS scores_models (
+     id SERIAL PRIMARY KEY,
+     model_name_id SMALLINT NOT NULL,
+     best_score SMALLINT NOT NULL,
+     best_model BYTEA NOT NULL,
+     param_id SMALLINT NOT NULL,
+     FOREIGN KEY(param_id) REFERENCES params(id),
+     FOREIGN KEY(model_name_id) REFERENCES model_names(id)
+    );
+    """
+
+    insert_name = """
+    INSERT INTO model_names(model_name) 
+        SELECT name
+    WHERE NOT EXISTS (
+        SELECT 1 FROM model_names WHERE model_name=name
+    );
+    """
+
+    commands = [
+        sql_params_table,
+        sql_names_table,
+        sql_main_table,
+        insert_name
+        ]
+
+    list_names = df_models['model_name'].to_list()
+
+    for i, sql in enumerate(commands):
+        if i != len(commands)-1:
+            cur.execute(sql)
+        else:
+            for name in list_names:
+                cur.execute(sql, (name,))
+
+
+    # close communication with the PostgreSQL database server
+    cur.close()
+    # commit the changes
+    conn.commit()
+
+    return df_models
+
 # STATING CSS
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
@@ -175,6 +266,7 @@ if use_current_data == "No":
                 RM.best_model,
                 show_df
             ),
+            create_db_tables,
             df_to_dict,
             save_all_or_one
         )
@@ -206,6 +298,7 @@ if use_current_data == "No":
                 RM.best_model,
                 show_df
             ),
+            create_db_tables,
             df_to_dict,
             save_all_or_one
         )
@@ -240,6 +333,7 @@ else:
             RM.best_model,
             show_df,
         ),
+        create_db_tables,
         df_to_dict,
         save_all_or_one
     )
