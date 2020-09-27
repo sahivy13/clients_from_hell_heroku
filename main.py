99a6238bd5ec4d2c843a26705e32e566d0292c5a
sahivy13 <sahivy1990@gmail.com>
2020-09-27 04:06:50 -0400

    fixing syntax

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ def create_db_tables(df_models):
 
     insert_name = """
     INSERT INTO model_names(model_name) 
-    VALUES(name)
+    VALUES (name)
     ON CONFLICT (model_name) DO NOTHING;
     """
 
